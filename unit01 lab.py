9eb9293a5e439ebeadb509e456d008cd8cf36a87
Ashton <ashtonwcoplin@gmail.com>
2024-09-02 23:56:57 -0500

    Add files via upload

---
+++ unit01 lab.py
@@ -0,0 +1,9 @@
+# This program displays a triangles area given a user-entered base and height.
+# @author Ashton Coplin
+
+#Get base and height
+base = int(input("Triangle base (inches): "))
+height = int(input("Triangle height (inches): "))
+
+# Display the area
+print("Area: ", (base*height)/2)
